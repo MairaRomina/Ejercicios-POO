feed1e5a27f8dd6d5387ec061bc5e436c0401f3e
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-04-25 19:10:03 -0300

    Update claseMenu.py

--- Ej3/claseMenu.py
+++ Ej3/claseMenu.py
@@ -1,8 +1,38 @@
 from claseManRegitro import Manejador
+from os import system
 
 class Menu:
-    __op = int
+    __switcher = None
 
     def __init__(self):
-        self.__op = None
+        self.__switcher = {
+            '1': self.opcion1,
+            '2': self.opcion2,
+            '3': self.opcion3,
+            '4': self.salir
+        }
+
+    def opcion (self, op, objetoLista):
+        func = self.__switcher.get( op, lambda: print('Opcion no valida') )
+        if op == '1' or op == '2' or op == '3':
+            func( objetoLista )
+        else:
+            func()
+
+    def opcion1 (self, objetoLista):  #mostrar variables por c/dia y hora
+        if type( objetoLista ) == Manejador:
+            objetoLista.mostrarDH()
+        else:
+            print('Error de tipo en menu')
+
+    def opcion2 (self, objetoLista): #muestra la temperatura promedio
+        if type( objetoLista ) == Manejador:
+            objetoLista.promedioTemp()
+
+    def opcion3 (self): #dado un dia muestra por hora variables
+        print('opcion 3 en el menu ')
+
+    def salir (self):
+        system('clear')
+        print('Salir')
         
