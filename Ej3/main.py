bf17769373b0a2010daaa3196819fe85076ca57a
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-04-25 19:11:04 -0300

    Update main.py

--- Ej3/main.py
+++ Ej3/main.py
@@ -1,9 +1,26 @@
 from claseManRegitro import Manejador
 from claseMenu import Menu
-from claseRegistro import Registro
+from os import system
 
 
 if __name__ == '__main__':
     listaRegistros = Manejador()
+    listaRegistros.agregarRegistro()
     listaRegistros.test()
-    print( listaRegistros )
+    print( listaRegistros ) #muestra la lista
+
+    menu = Menu()
+
+    op = 0
+    while op != 4:
+        print('--------------- Menu de opciones ---------------')
+        print('1. Mostrar max y min por cada dia')
+        print('2. Temperatura promedio por hora')
+        print('1. Listar informacion por dia')
+        print('4. Salir')
+        op =  input('Opcion: ')
+        menu.opcion( op, listaRegistros)
+
+    else:
+        system('clear')
+        print('Adios')
