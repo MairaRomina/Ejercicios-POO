d19fdaad29223d41ece74c6bd1c59ae036e0540c
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-04-28 18:37:17 -0300

    Update claseManViajero.py

--- Ej6/claseManViajero.py
+++ Ej6/claseManViajero.py
@@ -33,26 +33,39 @@ class Manejador:
                 self.agregarViajero( unViajero )
         archivo.close()
 
-    def buscarIndice(self, numero): #busca viajero por numero de ID
-        if type( numero ) == int:
-            for indice, viajero in enumerate( self.__lista ):
-                if viajero.getIdentificador() == numero:
-                    #print('manejador ',indice)
-                    objeto = self.buscarViajero( indice )
-                    return objeto
-        else:
-            print('Error de tipo en buscar viajero')        
+    def buscarIndice(self, clave):
+        i = 0
+        while i < len(self.__lista):
+            if self.__lista[i].getIdentificador() == clave:
+                return i
+            i = i + 1        
             
-    def buscarViajero (self, indice): #dado un indice devuelve el objeto solicitado
-        viajero = self.__lista[ indice ]
-        return viajero
+    def getViajero (self, indice): #dado un indice devuelve el objeto solicitado
+        return self.__lista[ indice ]
     
-    def comparar (self): #llama la funcion __gt__
+    def maximo (self): 
         max = self.__lista[0]
         for indice in range( len( self.__lista ) ):
             i = indice + 1
             if i < len(self.__lista):                                
-                '''punto a revisar'''
-                if self.__lista[i] > max: #devuelve un mayor unico
+                if self.__lista[i] > max: #llama la funcion __gt__
                     max = self.__lista[i]
-        print('Pasajero ID: {} tiene {} mayor cantidad de millas'.format( self.__lista[i].getIdentificador(), max) )            
+        return max.getcantidadMillas() #una vez obtenido el mayor
+
+    def buscarMayor (self, max): #llamo a la funcion para buscar los que fueron iguales al mayor
+        i = 0
+        while i < len(self.__lista):
+            if self.__lista[i].getcantidadMillas() == max:
+                print('El pasajero {} tiene mayor cantidad de millas, con un valor de: {}'.format( self.__lista[i].getIdentificador(), self.__lista[i].getcantidadMillas() ))
+            i = i + 1
+
+    def acumularMillas (self, acum, viajero): #opcion2
+        viajero = viajero + acum
+        print('Valor de millas actualizado con exito!')
+
+    def canjearMillas(self, canje, viajero ):
+        if viajero >= canje:
+            viajero = viajero - canje
+            print('Valores de millas actualizado, el canje se realizo con exito!')
+        else:
+            print('Error al canjear millas, no posee las suficientes para canjear')         
