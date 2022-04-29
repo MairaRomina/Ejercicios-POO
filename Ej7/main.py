39e0376144f8607342c141ae6a5d78a368945e7d
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-04-29 15:15:41 -0300

    Add files via upload

---
+++ Ej7/main.py
@@ -0,0 +1,23 @@
+from claseManViajero import Manejador
+from claseMenu import Menu
+
+if __name__ == '__main__':
+    listaViajero = Manejador()
+    listaViajero.test()
+    #print( listaViajero )
+    
+    menu = Menu()
+    
+    salir = True
+    while salir:
+        print('-------------------- MENU --------------------')
+        print('1. Consultar cantidad de millas')
+        print('2. Acumular millas')
+        print('3. Canjear millas')
+        print('4. Salir')
+        
+        op = input('Opcion: ')
+        menu.opcion( op, listaViajero )
+        #print( listaViajero )
+    # else:
+    #     print('Adios!')
